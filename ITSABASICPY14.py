8e7ac86366bffce58f4002f684466b55836fc253
oldjeff131 <oldjeff131@gmail.com>
2022-04-17 00:01:26 +0800

    finish 6 7

---
+++ ITSABASICPY14.py
@@ -0,0 +1,19 @@
+a=list(input())
+print(len(a))
+# if len(a)%2==0:
+#     for i in range(0,int((len(a)+1)/2),1):
+#         if a[i]!=a[len(a)-i]:
+#             print("NO")
+#             break
+#         elif i==len(a):
+#             print("YES")
+#             break
+# else:
+#     for i in range(0,int((len(a)+1)/2),1):
+#         if i!=len(a)/2:
+#             if a[i]!=a[len(a)-i]:
+#                 print("NO")
+#                 break
+#             elif i==len(a):
+#                 print("YES")
+#                 break
